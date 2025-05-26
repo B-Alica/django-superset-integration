90939e28a7582e2fc7d923341e09d88c2d9563c5
Alica Burlot <alica.burlot@developpement-durable.gouv.fr>
2025-05-26 10:34:43 +0200

    fix guest token url

--- build/lib/django_superset_integration/urls.py
+++ build/lib/django_superset_integration/urls.py
@@ -4,7 +4,7 @@ from .fetch_superset_guest_token import fetch_superset_guest_token
 
 urlpatterns = [
     path(
-        "/",
+        "guest_token/<slug:dashboard_id>",
         fetch_superset_guest_token,
         name="guest-token",
     ),
